0fc2233e2791242bea6438fc5c154876b4b01318
JovanStojakovic <jovanstojakovic33@gmail.com>
2023-05-02 01:53:21 +0200

    login,registracija,home page, navbar

---
+++ Android Kviz 2023/app/src/main/java/com/androidkviz/LogUserActivity.java
@@ -0,0 +1,22 @@
+package com.androidkviz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+
+
+public class LogUserActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_user);
+
+
+
+
+    }
+}
